2db487651cecd7169ae9f19dd1e5fd4e76c79044
Talaar Rastguelenian <tlr24@njit.edu>
2021-12-10 19:34:12 -0500

    Added missing initial CSV code to main

--- calculator/utilities/csv_reader.py
+++ calculator/utilities/csv_reader.py
@@ -1,11 +1,8 @@
 """Utility script for pandas csv_reader"""
-from datetime import datetime
 import pandas as pd
 import calculator.utilities.absolute_path as ap
-from calculator.utilities.file_writer import FileWriter
-from calculator.calculator import Calculator
 
-class CsvReader: # pylint: disable=too-few-public-methods,pointless-string-statement,consider-using-enumerate,line-too-long
+class CsvReader: # pylint: disable=pointless-string-statement,consider-using-enumerate,line-too-long,unused-variable,too-few-public-methods
     """Class for reading csv's"""
 
     @staticmethod
@@ -14,28 +11,3 @@ class CsvReader: # pylint: disable=too-few-public-methods,pointless-string-state
         csv = pd.read_csv(ap.absolute_path(path))
         data_frame = pd.DataFrame(csv)
         return data_frame
-
-    @staticmethod
-    def read_csv_dict(data_frame: pd.DataFrame):
-        """Take in a data frame and return it as a dictionary"""
-        return data_frame.to_dict(orient='index')
-
-    @staticmethod
-    def parse_addition_dict(addition_dict: dict): # pylint: disable=consider-using-enumerate,line-too-long
-        """Go through a dictionary derived from a csv and add all of the values per row"""
-        output_string = 'time,result\n'
-        for i in range(len(addition_dict)):
-            num_list = []
-            for j in addition_dict[i].keys():
-                print(addition_dict[i][j])
-                if j != "total":
-                    num_list.append(addition_dict[i][j])
-            Calculator.addition(tuple(num_list))
-            output_string += str(datetime.utcnow().timestamp()) + ',' + str(Calculator.get_last_result_value()) + '\n'
-        FileWriter.write_to_file('tests/test_data/test.csv', output_string)
-        return output_string
-
-    """@staticmethod
-    def to_string(data_frame: pd.DataFrame):
-        # Returns the data frame contents as a string
-        return data_frame.to_string()"""
---
+++ calculator/utilities/logger.py
@@ -0,0 +1,75 @@
+"""Utility script for logging csv operation results"""
+from datetime import datetime
+#import pytest
+import calculator.utilities.file_name as fn
+from calculator.utilities.file_writer import FileWriter
+from calculator.utilities.csv_reader import CsvReader
+#from calculator.utilities.exception_logger import ExceptionLogger
+from calculator.calculator import Calculator
+
+class Logger: # pylint: disable=unused-variable,line-too-long,pointless-string-statement
+    """Class for parsing and logging results from csv files"""
+
+    @staticmethod
+    def parse_df_to_dict(read_path: str):  # pylint: disable=unused-variable,line-too-long
+        """Parsing a data frame derived from a csv into a dictionary before performing the operation"""
+        data_frame = CsvReader.read_csv(read_path)
+        output_dict = {'result': [], 'time': [], 'filename': [], 'recordNumber': [], 'operation': []}
+        for index, row in data_frame.iterrows():
+            value1 = row['value_1']
+            value2 = row['value_2']
+            nums = (value1, value2)
+            output_dict['result'].append(nums)
+            output_dict['time'].append(str(datetime.utcnow().timestamp()))
+            output_dict['filename'].append(fn.get_file_name(read_path))
+            output_dict['recordNumber'].append(index+1)
+        return output_dict
+
+    @staticmethod
+    def log_addition(read_path: str, write_path: str):  # pylint: disable=unused-variable
+        """Parsing a data frame derived from a csv and adding all of the values per row"""
+        data_dict = Logger.parse_df_to_dict(read_path)
+        for i in range(len(data_dict['result'])):
+            Calculator.addition(data_dict['result'][i])
+            data_dict['result'][i] = Calculator.get_last_result_value()
+            data_dict['operation'].append("addition")
+        FileWriter.write_csv(write_path, data_dict)
+
+    @staticmethod
+    def log_subtraction(read_path: str, write_path: str):  # pylint: disable=unused-variable
+        """Parsing a data frame derived from a csv and subtracting all of the values per row"""
+        data_dict = Logger.parse_df_to_dict(read_path)
+        for i in range(len(data_dict['result'])):
+            Calculator.subtraction(data_dict['result'][i])
+            data_dict['result'][i] = Calculator.get_last_result_value()
+            data_dict['operation'].append("subtraction")
+        FileWriter.write_csv(write_path, data_dict)
+
+    @staticmethod
+    def log_multiplication(read_path: str, write_path: str):  # pylint: disable=unused-variable
+        """Parsing a data frame derived from a csv and multiplying all of the values per row"""
+        data_dict = Logger.parse_df_to_dict(read_path)
+        for i in range(len(data_dict['result'])):
+            Calculator.multiplication(data_dict['result'][i])
+            data_dict['result'][i] = Calculator.get_last_result_value()
+            data_dict['operation'].append("multiplication")
+        FileWriter.write_csv(write_path, data_dict)
+
+    @staticmethod
+    def log_division(read_path: str, write_path: str):  # pylint: disable=unused-variable
+        """Parsing a data frame derived from a csv and dividing all of the values per row"""
+        data_dict = Logger.parse_df_to_dict(read_path)
+        print(len(data_dict['result']))
+        for i in range(len(data_dict['result'])):
+            print(data_dict['result'][i])
+            '''
+            for n in range(len(data_dict['result'][i])):
+                if n != 0 and data_dict['result'][i][n] == 0:
+                    ExceptionLogger.write_to_exceptions(read_path,i)
+                    break
+                    # send record number (i) and file name to exemptions log file
+            '''
+            Calculator.division(data_dict['result'][i])
+            data_dict['result'][i] = Calculator.get_last_result_value()
+            data_dict['operation'].append("division")
+        FileWriter.write_csv(write_path, data_dict)
