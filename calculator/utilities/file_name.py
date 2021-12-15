ab637078469e93fb18ed1d523bfd3c71faaa5cdc
tlr24 <70588311+tlr24@users.noreply.github.com>
2021-12-15 17:48:37 -0500

    Deleted no longer used file_name.py

--- calculator/utilities/file_name.py
+++
@@ -1,9 +0,0 @@
-"""Script that holds get file name utility function"""
-import os
-import calculator.utilities.absolute_path as ap
-
-
-def get_file_name(filepath: str):
-    """Method to get file name from a path"""
-    file_name = os.path.basename(ap.absolute_path(filepath))
-    return file_name
